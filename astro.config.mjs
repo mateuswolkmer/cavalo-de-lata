74c7af85c69ccf6bfa82cccf4a0d74747730d698
Mateus Wolkmer <mateuswsouza@gmail.com>
2025-04-28 00:19:40 -0300

    refactor: tina + biome init

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tinaDirective from './astro-tina-directive/register';
+
+// https://astro.build/config
+export default defineConfig({
+	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
+	integrations: [mdx(), sitemap(), react(), tinaDirective()],
+});
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,16 @@
+import { getCollection } from 'astro:content';
+import rss from '@astrojs/rss';
+import config from '../content/config/config.json';
+
+export async function GET(context) {
+	const posts = await getCollection('blog');
+	return rss({
+		title: config.seo.title,
+		description: config.seo.description,
+		site: context.site,
+		items: posts.map((post) => ({
+			...post.data,
+			link: `/blog/${post.id}/`,
+		})),
+	});
+}
